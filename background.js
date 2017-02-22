93d7c7a99e2babe4325b8fe2b3fc8dd07fc3feae
frkandris <p.toth.andras@gmail.com>
2017-02-22 11:00:53 +0100

    1.1 - click on icon takes to rank2traffic.com website, small refactor
    1.1 - click on icon takes to rank2traffic.com website, small refactor

--- background.js
+++ background.js
@@ -1,74 +1,87 @@
 // Based on the CLD plugin by Chromium Authors.
 
-var selectedId = -1;
-function getTrafficAndSetBadge() {
+var selectedTabId = -1;
 
-  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+function getDomainFromURL(url) {
 
       // parse the root domain from the url
-      var domain = psl.parse(tabs[0].url.split("/")[2]).domain;
+      var domain = psl.parse(url.split("/")[2]).domain;
 
       // if there is no domain, do nothing
       if (domain == null) {
         return true;
+      } else {
+        return domain;
       }
 
-      // if we don't have it in the local cache storage...
-      if (!localStorage.getCacheItem(domain)) {       
+}
 
-        // query it from rank2traffic.com
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'http://www.rank2traffic.com/'+domain, true);
+function getTrafficAndSetBadge() {
 
-        xhr.onload = function(e) {
-          var webpageSource = this.responseText;
-          var regExp = /infobox-data-number..(.*)\</g;
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
 
-          // we need the 2nd occurence
-          match = regExp.exec(webpageSource);
-          match = regExp.exec(webpageSource);
-          var count = match[1];
+    var domain = getDomainFromURL(tabs[0].url);
 
-          // make it shorter
-          count = count.replace(" Thousand", "k");
-          count = count.replace(" Million", "M");
-          count = count.replace(" Billion", "B");
-          count = count.replace(" ", "");
+    // if we don't have it in the local cache storage...
+    if (!localStorage.getCacheItem(domain)) {       
 
-          // set the badge and tooltip text
-          chrome.browserAction.setBadgeText({"text": count, tabId: selectedId});
-          chrome.browserAction.setTitle({"title": count, tabId: selectedId});
+      // query it from rank2traffic.com
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'http://www.rank2traffic.com/'+domain, true);
 
-          // save it to the local storage
-          localStorage.setCacheItem(domain, count, { days: 7 });
-        }
-        xhr.send();
-      } else {
+      xhr.onload = function(e) {
+        var webpageSource = this.responseText;
+        var regExp = /infobox-data-number..(.*)\</g;
 
-        // if we have it in the local storage, load it
-        var count = localStorage.getCacheItem(domain);
+        // we need the 2nd occurence
+        match = regExp.exec(webpageSource);
+        match = regExp.exec(webpageSource);
+        var count = match[1];
+
+        // make it shorter
+        count = count.replace(" Thousand", "k");
+        count = count.replace(" Million", "M");
+        count = count.replace(" Billion", "B");
+        count = count.replace(" ", "");
 
         // set the badge and tooltip text
-        chrome.browserAction.setBadgeText({"text": count, tabId: selectedId});
-        chrome.browserAction.setTitle({"title": count, tabId: selectedId});        
+        chrome.browserAction.setBadgeText({"text": count, tabId: selectedTabId});
+        chrome.browserAction.setTitle({"title": count, tabId: selectedTabId});
+
+        // save it to the local storage
+        localStorage.setCacheItem(domain, count, { days: 3 });
       }
+      xhr.send();
+    } else {
+
+      // if we have it in the local storage, load it
+      var count = localStorage.getCacheItem(domain);
+
+      // set the badge and tooltip text
+      chrome.browserAction.setBadgeText({"text": count, tabId: selectedTabId});
+      chrome.browserAction.setTitle({"title": count, tabId: selectedTabId});        
+    }
   });
 }
 
 // initiate listeners
 
 chrome.tabs.onUpdated.addListener(function(tabId, props) {
-  if (props.status == "complete" && tabId == selectedId)
+  if (props.status == "complete" && tabId == selectedTabId)
     getTrafficAndSetBadge();
 });
 
 chrome.tabs.onSelectionChanged.addListener(function(tabId, props) {
-  selectedId = tabId;
+  selectedTabId = tabId;
   getTrafficAndSetBadge();
 });
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  selectedId = tabs[0].id;
+  selectedTabId = tabs[0].id;
   getTrafficAndSetBadge();
 });
 
+chrome.browserAction.onClicked.addListener(function(tab) {
+    var domain = getDomainFromURL(tab.url);
+    chrome.tabs.create({ url: 'http://www.rank2traffic.com/'+domain });
+});
